0e8ddcb3c14a13beb8afb6f150288c5e9b122a22
jgjgill <79239852+jgjgill@users.noreply.github.com>
2022-10-21 10:28:18 +0900

    221021

---
+++ programmers/132265.js
@@ -0,0 +1,28 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/132265
+
+function solution(topping) {
+  const a = new Set();
+  const b = {};
+
+  let answer = 0;
+  let check = 0;
+
+  for (let i = 0; i < topping.length; i++) {
+    if (b[topping[i]]) {
+      b[topping[i]]++;
+    } else {
+      b[topping[i]] = 1;
+      check++;
+    }
+  }
+
+  for (let i = 0; i < topping.length; i++) {
+    a.add(topping[i]);
+    b[topping[i]]--;
+
+    if (!b[topping[i]]) check--;
+    if (a.size === check) answer++;
+  }
+
+  return answer;
+}
