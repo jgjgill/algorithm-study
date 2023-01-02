55eaa7ca826b687c9e4bd650a21204752a775338
jgjgill <79239852+jgjgill@users.noreply.github.com>
2023-01-02 12:56:37 +0900

    230102

---
+++ leetcode/520.js
@@ -0,0 +1,16 @@
+// https://leetcode.com/problems/detect-capital/
+
+/**
+ * @param {string} word
+ * @return {boolean}
+ */
+var detectCapitalUse = function (word) {
+  if (word === word.toUpperCase() || word === word.toLowerCase()) return true
+  if (
+    word[0] === word[0].toUpperCase() &&
+    word.slice(1) === word.slice(1).toLowerCase()
+  )
+    return true
+
+  return false
+}
