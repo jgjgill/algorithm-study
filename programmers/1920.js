1e84f310ca95157aac33726476521204c82f9bd8
jgjgill <79239852+jgjgill@users.noreply.github.com>
2023-01-07 23:02:50 +0900

    230107

---
+++ programmers/1920.js
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/build-array-from-permutation/
+
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var buildArray = function(nums) {
+  return nums.map((num) => nums[num])
+};
