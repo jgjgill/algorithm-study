c2d6f2206d0a81db0f94adda11343b4ca051a4dc
jgjgill <79239852+jgjgill@users.noreply.github.com>
2023-04-09 23:59:42 +0900

    230409

--- programmers/42746.js
+++ programmers/42746.js
@@ -8,6 +8,18 @@ function solution(numbers) {
       if (a + b > b + a) return -1
       if (a + b === b + a) return 0
     })
-    .join('')
-    .replace(/^0*$/, '0')
+    .join("")
+    .replace(/^0*$/, "0")
+}
+
+function solution(numbers) {
+  const answer = numbers
+    .map(String)
+    .sort((a, b) => {
+      if (a + b < b + a) return 1
+      if (a + b > b + a) return -1
+      if (a + b === b + a) return 0
+    })
+    .join("")
+  return answer[0] === "0" ? "0" : answer
 }
