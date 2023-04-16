9b71450497d4473cc14be49f9c1f8aeffd1afd02
jgjgill <79239852+jgjgill@users.noreply.github.com>
2023-04-16 23:58:14 +0900

    230416

--- programmers/84512.js
+++ programmers/84512.js
@@ -1,21 +1,40 @@
 // https://school.programmers.co.kr/learn/courses/30/lessons/84512
 
 function solution(word) {
-  const alphabets = ['A', 'E', 'I', 'O', 'U']
+  const alphabets = ["A", "E", "I", "O", "U"]
   let index = 0
-  
+
   function dfs(now) {
-      index++
-      
-      if (now === word) return true
-      if (now.length === 5) return false
-      
-      for (const alphabet of alphabets) {
-          if(dfs(now + alphabet)) return index
-      }
+    index++
+
+    if (now === word) return true
+    if (now.length === 5) return false
+
+    for (const alphabet of alphabets) {
+      if (dfs(now + alphabet)) return index
+    }
   }
-  
+
   for (const alphabet of alphabets) {
-      if (dfs(alphabet)) return index
+    if (dfs(alphabet)) return index
+  }
+}
+
+function solution(word) {
+  const alphabets = ["A", "E", "I", "O", "U"]
+  let index = 0
+
+  for (const alphabet of alphabets) {
+    if (dfs(alphabet)) return index
+  }
+
+  function dfs(check) {
+    index += 1
+    if (check === word) return true
+    if (check.length === 5) return false
+
+    for (const alphabet of alphabets) {
+      if (dfs(check + alphabet)) return index
+    }
   }
 }
