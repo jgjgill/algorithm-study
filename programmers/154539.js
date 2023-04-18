a1dbd5c74525bd35e07ea107d015e66fe2e7f9e0
jgjgill <79239852+jgjgill@users.noreply.github.com>
2023-04-18 23:13:50 +0900

    230418

--- programmers/154539.js
+++ programmers/154539.js
@@ -14,3 +14,27 @@ function solution(numbers) {
 
   return answer
 }
+
+function solution(numbers) {
+  const stack = []
+  const answer = []
+
+  numbers.forEach((number, index) => {
+    if (stack.length === 0) stack.push([number, index])
+    else {
+      while (stack.length !== 0 && stack.at(-1)[0] < number) {
+        const cur = stack.pop()
+
+        answer.push([number, cur[1]])
+      }
+      stack.push([number, index])
+    }
+  })
+
+  while (stack.length !== 0) {
+    const cur = stack.pop()
+    answer.push([-1, cur[1]])
+  }
+
+  return answer.sort((a, b) => a[1] - b[1]).map((item) => item[0])
+}
