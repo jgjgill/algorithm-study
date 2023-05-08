337b985a53f6298272258b10742c927daef6c534
jgjgill <79239852+jgjgill@users.noreply.github.com>
2023-05-08 20:50:13 +0900

    230508

---
+++ programmers/43238.js
@@ -0,0 +1,19 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/43238
+
+function solution(n, times) {
+  let left = 1
+  let right = Math.max(...times) * n
+
+  while (left <= right) {
+    const mid = Math.floor((left + right) / 2)
+    const check = times.reduce((acc, cur) => acc + Math.floor(mid / cur), 0)
+
+    if (check >= n) {
+      right = mid - 1
+    } else {
+      left = mid + 1
+    }
+  }
+
+  return left
+}
