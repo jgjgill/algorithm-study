52e1798b8dda0c258385e67adc8b1fcdd61a3e13
jgjgill <dbdltm22@naver.com>
2023-06-05 21:00:25 +0900

    230605

--- programmers/42579.js
+++ programmers/42579.js
@@ -1,43 +1,71 @@
 // https://school.programmers.co.kr/learn/courses/30/lessons/42579
 
 function solution(genres, plays) {
-  const count_check = {}
-  const result_check = {}
-  const length = genres.length
-  const temp = []
-  const answer = []
+  const count_check = {};
+  const result_check = {};
+  const length = genres.length;
+  const temp = [];
+  const answer = [];
 
   for (let i = 0; i < length; i++) {
-    const genre = genres[i]
-    const play = plays[i]
+    const genre = genres[i];
+    const play = plays[i];
 
     if (count_check[genre] === undefined) {
-      count_check[genre] = play
-      result_check[genre] = 0
+      count_check[genre] = play;
+      result_check[genre] = 0;
     } else {
-      count_check[genre] += play
+      count_check[genre] += play;
     }
   }
 
   for (let i = 0; i < length; i++) {
-    const genre = genres[i]
-    const play = plays[i]
+    const genre = genres[i];
+    const play = plays[i];
 
-    temp.push([count_check[genre], play, i, genre])
+    temp.push([count_check[genre], play, i, genre]);
   }
 
   temp.sort((a, b) => {
-    if (a[0] !== b[0]) return b[0] - a[0]
-    else if (a[1] !== b[1]) return b[1] - a[1]
-    return a[2] - b[2]
-  })
+    if (a[0] !== b[0]) return b[0] - a[0];
+    else if (a[1] !== b[1]) return b[1] - a[1];
+    return a[2] - b[2];
+  });
 
   temp.forEach((item) => {
     if (result_check[item[3]] !== 2) {
-      answer.push(item[2])
-      result_check[item[3]] += 1
+      answer.push(item[2]);
+      result_check[item[3]] += 1;
     }
-  })
+  });
 
-  return answer
+  return answer;
+}
+
+function solution(genres, plays) {
+  const check = {};
+  const length = genres.length;
+  const answer = [];
+
+  for (let i = 0; i < length; i++) {
+    const genre = genres[i];
+    const play = plays[i];
+
+    if (!(genre in check)) check[genre] = [0, []];
+    check[genre][0] += play;
+    check[genre][1].push([play, i]);
+  }
+
+  const check_values = Object.values(check);
+
+  check_values.sort((a, b) => b[0] - a[0]);
+
+  for (const [sum, data] of check_values) {
+    data.sort((a, b) => b[0] - a[0]).splice(2);
+    for (const [play, index] of data) {
+      answer.push(index);
+    }
+  }
+
+  return answer;
 }
--- programmers/42587.js
+++ programmers/42587.js
@@ -1,44 +1,104 @@
 // https://school.programmers.co.kr/learn/courses/30/lessons/42587
 
 function solution(priorities, location) {
-  const stack = []
-  let index = 0
+  const stack = [];
+  let index = 0;
 
   while (true) {
-    let check = true
+    let check = true;
 
     for (let i = index + 1; i < priorities.length; i++) {
       if (priorities[index] < priorities[i]) {
-        if (index === location) location = priorities.length
+        if (index === location) location = priorities.length;
 
-        priorities.push(priorities[index])
-        check = false
-        break
+        priorities.push(priorities[index]);
+        check = false;
+        break;
       }
     }
 
-    if (check) stack.push(priorities[index])
-    if (index === location) return stack.length
+    if (check) stack.push(priorities[index]);
+    if (index === location) return stack.length;
 
-    index++
+    index++;
   }
 }
 
 function solution(priorities, location) {
-  const stack = []
-  const length = priorities.length
-  priorities = priorities.map((priority, index) => [priority, index])
+  const stack = [];
+  const length = priorities.length;
+  priorities = priorities.map((priority, index) => [priority, index]);
 
   while (stack.length !== length) {
-    stack.push(priorities.shift())
+    stack.push(priorities.shift());
 
     for (let i = 0; i < priorities.length; i++) {
       if (stack.at(-1)[0] < priorities[i][0]) {
-        priorities.push(stack.pop())
-        break
+        priorities.push(stack.pop());
+        break;
       }
     }
   }
 
-  return stack.findIndex((item) => item[1] === location) + 1
+  return stack.findIndex((item) => item[1] === location) + 1;
+}
+
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Queue {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+  }
+
+  enqueue(newValue) {
+    const newNode = new Node(newValue);
+    if (this.head === null) {
+      this.head = this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+  }
+
+  dequeue() {
+    const value = this.head.value;
+    this.head = this.head.next;
+    return value;
+  }
+
+  peek() {
+    return this.head.value;
+  }
+}
+
+function solution(priorities, location) {
+  const queue = new Queue();
+  let count = 0;
+
+  for (let i = 0; i < priorities.length; i++) {
+    queue.enqueue([priorities[i], i]);
+  }
+
+  priorities.sort((a, b) => b - a);
+
+  while (true) {
+    const currentValue = queue.peek();
+    if (currentValue[0] < priorities[count]) {
+      queue.enqueue(queue.dequeue());
+    } else {
+      const value = queue.dequeue();
+      count += 1;
+      if (location === value[1]) {
+        return count;
+      }
+    }
+  }
+
+  return count;
 }
