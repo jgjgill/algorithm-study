14c8b0c849760fd8a633426833e89a0d4f18330a
jgjgill <dbdltm22@naver.com>
2023-06-07 22:48:32 +0900

    230607

--- programmers/42746.js
+++ programmers/42746.js
@@ -4,22 +4,28 @@ function solution(numbers) {
   return numbers
     .map(String)
     .sort((a, b) => {
-      if (a + b < b + a) return 1
-      if (a + b > b + a) return -1
-      if (a + b === b + a) return 0
+      if (a + b < b + a) return 1;
+      if (a + b > b + a) return -1;
+      if (a + b === b + a) return 0;
     })
     .join("")
-    .replace(/^0*$/, "0")
+    .replace(/^0*$/, "0");
 }
 
 function solution(numbers) {
   const answer = numbers
     .map(String)
     .sort((a, b) => {
-      if (a + b < b + a) return 1
-      if (a + b > b + a) return -1
-      if (a + b === b + a) return 0
+      if (a + b < b + a) return 1;
+      if (a + b > b + a) return -1;
+      if (a + b === b + a) return 0;
     })
-    .join("")
-  return answer[0] === "0" ? "0" : answer
+    .join("");
+  return answer[0] === "0" ? "0" : answer;
+}
+
+function solution(numbers) {
+  const answer = numbers.map(String).sort((a, b) => b + a - (a + b));
+
+  return answer[0] === "0" ? "0" : answer.join("");
 }
--- programmers/42883.js
+++ programmers/42883.js
@@ -1,27 +1,42 @@
 // https://school.programmers.co.kr/learn/courses/30/lessons/42883
 
 function solution(number, k) {
-  const answer = []
-  let count = 0
-  let index = -1
+  const answer = [];
+  let count = 0;
+  let index = -1;
 
   outer: while (answer.length !== number.length - k) {
-    index += 1
+    index += 1;
 
-    if (number[index] === '9') {
-      answer.push(number[index])
-      continue outer
+    if (number[index] === "9") {
+      answer.push(number[index]);
+      continue outer;
     }
 
     for (let i = index; i <= index + k - count; i++) {
       if (number[index] < number[i]) {
-        count += 1
-        continue outer
+        count += 1;
+        continue outer;
       }
     }
 
-    answer.push(number[index])
+    answer.push(number[index]);
   }
 
-  return answer.join('')
+  return answer.join("");
+}
+
+function solution(number, k) {
+  const stack = [];
+
+  for (const num of number) {
+    while (k > 0 && stack.at(-1) < num) {
+      stack.pop();
+      k -= 1;
+    }
+
+    stack.push(num);
+  }
+
+  return stack.splice(0, stack.length - k).join("");
 }
