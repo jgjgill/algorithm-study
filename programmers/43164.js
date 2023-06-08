074b0dc4501c24a6c65cb72be550652ee96b0e16
jgjgill <dbdltm22@naver.com>
2023-06-08 23:33:57 +0900

    230608

--- programmers/43164.js
+++ programmers/43164.js
@@ -1,32 +1,63 @@
 // https://school.programmers.co.kr/learn/courses/30/lessons/43164
 
 function solution(tickets) {
-  const visit = Array.from({ length: tickets.length }, () => false)
-  const temp = ["ICN"]
-  let finish = false
-  let answer
+  const visit = Array.from({ length: tickets.length }, () => false);
+  const temp = ["ICN"];
+  let finish = false;
+  let answer;
 
-  tickets.sort()
-  dfs("ICN", 0)
+  tickets.sort();
+  dfs("ICN", 0);
 
-  return answer
+  return answer;
 
   function dfs(start_air, count) {
-    if (finish) return
+    if (finish) return;
     if (count === tickets.length) {
-      answer = [...temp]
-      finish = true
-      return
+      answer = [...temp];
+      finish = true;
+      return;
     }
 
     for (let i = 0; i < tickets.length; i++) {
-      if (visit[i]) continue
-      if (tickets[i][0] !== start_air) continue
-      temp.push(tickets[i][1])
-      visit[i] = true
-      dfs(tickets[i][1], count + 1)
-      temp.pop()
-      visit[i] = false
+      if (visit[i]) continue;
+      if (tickets[i][0] !== start_air) continue;
+      temp.push(tickets[i][1]);
+      visit[i] = true;
+      dfs(tickets[i][1], count + 1);
+      temp.pop();
+      visit[i] = false;
+    }
+  }
+}
+
+function solution(tickets) {
+  const visit = Array.from({ length: tickets.length }, () => false);
+  const temp = ["ICN"];
+  let finish = false;
+  let answer;
+
+  tickets.sort();
+  dfs("ICN", 0);
+
+  return answer;
+
+  function dfs(start_air, count) {
+    if (finish) return;
+    if (count === tickets.length) {
+      answer = [...temp];
+      finish = true;
+      return;
+    }
+
+    for (let i = 0; i < tickets.length; i++) {
+      if (visit[i]) continue;
+      if (tickets[i][0] !== start_air) continue;
+      temp.push(tickets[i][1]);
+      visit[i] = true;
+      dfs(tickets[i][1], count + 1);
+      temp.pop();
+      visit[i] = false;
     }
   }
 }
--- programmers/43238.js
+++ programmers/43238.js
@@ -1,19 +1,39 @@
 // https://school.programmers.co.kr/learn/courses/30/lessons/43238
 
 function solution(n, times) {
-  let left = 1
-  let right = Math.max(...times) * n
+  let left = 1;
+  let right = Math.max(...times) * n;
 
   while (left <= right) {
-    const mid = Math.floor((left + right) / 2)
-    const check = times.reduce((acc, cur) => acc + Math.floor(mid / cur), 0)
+    const mid = Math.floor((left + right) / 2);
+    const check = times.reduce((acc, cur) => acc + Math.floor(mid / cur), 0);
 
     if (check >= n) {
-      right = mid - 1
+      right = mid - 1;
     } else {
-      left = mid + 1
+      left = mid + 1;
     }
   }
 
-  return left
+  return left;
+}
+
+function solution(n, times) {
+  times.sort((a, b) => a - b);
+
+  let left = 1;
+  let right = times.at(-1) * n;
+
+  while (left <= right) {
+    let mid = Math.floor((left + right) / 2);
+
+    const check = times.reduce((acc, cur) => acc + Math.floor(mid / cur), 0);
+    if (check >= n) {
+      right = mid - 1;
+    } else {
+      left = mid + 1;
+    }
+  }
+
+  return left;
 }
