30a982a60d1a4c222846495d514f21018ad1c927
jgjgill <dbdltm22@naver.com>
2024-09-23 23:50:21 +0900

    240923

--- programmers/12980.js
+++ programmers/12980.js
@@ -14,3 +14,16 @@ function solution(n) {
 
   return answer;
 }
+
+function solution(n) {
+  let answer = 0
+  
+  while(n > 0) {
+      if (n % 2 === 1) {
+          answer += 1
+      }
+      n =  Math.floor(n / 2)
+  }
+  
+  return answer;
+}
