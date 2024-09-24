2fcfd3a861e50e06bed0dcb472a37a3c8ef9c1b2
jgjgill <dbdltm22@naver.com>
2024-09-24 19:32:31 +0900

    240924

--- programmers/12973.js
+++ programmers/12973.js
@@ -13,3 +13,19 @@ function solution(s) {
 
   return stack.length ? 0 : 1;
 }
+
+
+function solution(s) {
+  const stack = []
+  
+  for (let char of s) {        
+      if (stack.length > 0 && stack.at(-1) === char) {
+          stack.pop()
+      } else {
+          stack.push(char)
+      }
+  }
+
+  
+  return stack.length === 0 ? 1 : 0
+}
