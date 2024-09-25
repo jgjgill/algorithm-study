77c3569261f03deec4aa2192c6089702b4b5b3bf
jgjgill <dbdltm22@naver.com>
2024-09-25 19:11:27 +0900

    240925

--- programmers/77885.js
+++ programmers/77885.js
@@ -13,3 +13,28 @@ function solution(numbers) {
 
   return answer
 }
+
+function solution(numbers) {
+  const answer = [];
+  
+  for (let number of numbers) {
+      const bits = number.toString(2).split('')
+      const lastZeroIndex = bits.lastIndexOf('0')
+      
+      if (lastZeroIndex === -1) {
+          bits.splice(1, 0, '0')
+      } else {
+          const lastIndex = bits.length - 1
+          
+          if (bits[lastIndex] === '0') {
+              bits[lastIndex] = '1'
+          } else {
+              [bits[lastZeroIndex], bits[lastZeroIndex + 1]] = [bits[lastZeroIndex + 1], bits[lastZeroIndex]]
+          }
+      }
+      
+      answer.push(parseInt(bits.join(''), 2))
+  }
+  
+  return answer
+}
