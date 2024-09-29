ba0b7566bc7cb072e6f0d235182c5b10c6d427a3
jgjgill <dbdltm22@naver.com>
2024-09-29 23:47:25 +0900

    240929

--- programmers/12913.js
+++ programmers/12913.js
@@ -23,3 +23,17 @@ function solution(land) {
 
   return answer
 }
+
+function solution(land) {
+  let dp = Array.from({length: land.length}, () => [])
+  dp[0] =land[0]
+  
+  for (let i = 1; i < land.length; i++) {
+      dp[i][0] = land[i][0] + Math.max(dp[i - 1][1], dp[i - 1][2], dp[i - 1][3])
+      dp[i][1] = land[i][1] + Math.max(dp[i - 1][0], dp[i - 1][2], dp[i - 1][3])
+      dp[i][2] = land[i][2] + Math.max(dp[i - 1][0], dp[i - 1][1], dp[i - 1][3])
+      dp[i][3] = land[i][3] + Math.max(dp[i - 1][0], dp[i - 1][1], dp[i - 1][2])
+  }
+  
+  return Math.max(...dp.at(-1))
+}
--- programmers/12985.js
+++ programmers/12985.js
@@ -27,3 +27,17 @@ function solution(n, a, b) {
 
   return answer;
 }
+
+function solution(n,a,b) {
+  let answer = 0
+  
+  while(a !== b) {
+      a = Math.ceil(a / 2)
+      b = Math.ceil(b / 2)
+      answer += 1 
+  }
+  
+  return answer
+}
+
+
